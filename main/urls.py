516d1f87d4e9ccf0c3cd124268e37ee2d1394f75
Nyota254 <knyota66@gmail.com>
2022-03-05 11:53:35 +0300

    add full crud functionallity

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from main.views import (
-    StudentList
+    StudentList,
+    StudentDetail,
+    StudentCreate,
+    StudentUpdate,
+    StudentDelete
 )
 
 urlpatterns = [
-    path('',StudentList.as_view(),name="students")
+    path('',StudentList.as_view(),name="students"),
+    path('student/<int:pk>/',StudentDetail.as_view(),name="student"),
+    path('create-student/',StudentCreate.as_view(),name="create-student"),
+    path('update-student/<int:pk>',StudentUpdate.as_view(),name="update-student"),
+    path('delete-student/<int:pk>',StudentDelete.as_view(),name="delete-student")
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,35 @@
 from email.policy import HTTP
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import (
+                                        CreateView,
+                                        UpdateView,
+                                        DeleteView
+)
+from django.urls import reverse_lazy
 from main.models import Student
 
 class StudentList(ListView):
     model = Student
     context_object_name = 'students'
+
+class StudentDetail(DetailView):
+    model = Student
+    context_object_name = 'student'
+
+class StudentCreate(CreateView):
+    model = Student
+    fields = '__all__'
+    success_url = reverse_lazy('students')
+
+class StudentUpdate(UpdateView):
+    model = Student
+    fields = '__all__'
+    success_url = reverse_lazy('students')
+
+class StudentDelete(DeleteView):
+    model = Student
+    context_object_name = 'student'
+    success_url = reverse_lazy('students')
 
