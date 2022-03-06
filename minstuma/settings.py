abd2a1b0f779d89e283ffff55fae0ee7e5fd111b
Nyota254 <knyota66@gmail.com>
2022-03-06 10:40:04 +0300

    add minimal styling for usability

--- minstuma/settings.py
+++ minstuma/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main.apps.MainConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL ='login'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
